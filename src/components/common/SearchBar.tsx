719fc5690320694148ee2bafbf8bd236adf4ed76
devfaf <104205080+devfaf@users.noreply.github.com>
2026-07-01 19:49:24 +0330

    add search bar

---
+++ src/components/common/SearchBar.tsx
@@ -0,0 +1,18 @@
+type InputProps = {
+    type: 'search' | 'email' | 'password' | 'text';
+    id?:string;
+    name?:string;
+    required?:boolean;
+    minlength?:number;
+    maxlength?:number;
+    size?:number;
+}
+const SearchBar = ({type = "text", id, name, required, minlength, maxlength, size}: InputProps) => {
+  return (
+    <div>
+        <input type="text" type={type} id={id} name={name} required={required} minLength={minlength} maxLength={maxlength} size={size} />
+    </div>
+  )
+}
+
+export default SearchBar
