8288a6b4406a91774da049c73e6921f535a69181
devfaf <104205080+devfaf@users.noreply.github.com>
2026-07-04 21:43:08 +0330

    connet open new project button to modal

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -3,8 +3,9 @@ import { BsXLg } from "react-icons/bs";
 const Modal = ({ isOpen, children, onClose }: ModalProps) => {
 
     return (
-        <div onClick={onClose} className={`w-[100vw] h-[100vh] bg-black/30 fixed inset-0 duration-200 ${isOpen ? 'flex' : 'hidden'}`}>
-            <div className={`p-4 rounded-lg bg-gray-100 w-fit absolute inset-0 border-2 border-gray-300 transform -translate-x-1/2 -translate-y-1/2 top-1/2 and left-1/2`}>
+        <div className={`${isOpen ? 'flex' : 'hidden'}`}>
+            <div onClick={onClose} className={`w-[100vw] h-[100vh] bg-black/30 absolute fixed inset-0 duration-200`}></div>
+            <div className={`p-4 rounded-lg bg-gray-100 w-fit absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 right-1/2 border-2 border-gray-300`}>
                 <BsXLg onClick={onClose} className="absolute right-2 top-2 cursor-pointer" />
                 {children}
             </div>
--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,23 +1,31 @@
+import { useState } from "react"
 import Button from "../components/common/Button"
 import SearchBar from "../components/common/SearchBar"
 import FilterDropdown from "../components/common/FilterDropdown"
-
+import Modal from "../components/common/Modal"
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const addModalHandler = () => {
+    setIsOpen(true)
+  }
+
   return (
     <header className="flex gap-4 mx-auto p-3 bg-black">
       <div className="flex flex-col lg:flex-row lg:justify-end items-center gap-6 w-full bg-pink-400">
         <div className="flex gap-6 w-full items-center justify-between lg:justify-start">
           <h1>TaskFlow</h1>
           <FilterDropdown />
-          <Button className="bg-blue-500 hover:bg-blue-700 duration-300 rounded-lg p-2 text-white cursor-pointer">
+          <Button onClick={addModalHandler} className="bg-blue-500 hover:bg-blue-700 duration-300 rounded-lg p-2 text-white cursor-pointer">
             اضافه کردن
           </Button>
         </div>
         <SearchBar className={`bg-gray-100 border-2 border-gray-300 outline-none rounded-lg px-2 w-full`} type="text" />
 
       </div>
-
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        فرم افزودن پروژه
+      </Modal>
     </header>
   )
 }
