6d5146c301e875ed84ea4828a88c2fed495db6ce
devfaf <104205080+devfaf@users.noreply.github.com>
2026-07-14 15:48:10 +0330

    feat: add outlet for dashboard pages

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router";
 import DashboardLayout from "../layouts/DashboardLayout";
+import ProjectList from "../features/projects/components/ProjectList";
+import EmptyState from "../components/common/EmptyState";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <DashboardLayout />,
+        children: [
+            {
+                path: "projects",
+                element: <ProjectList />
+            },
+            {
+                path: "EmptyState",
+                element: <EmptyState />
+            }
+        ]
     },
     {
         path: "/projects",
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,13 +1,18 @@
 import DashboardPage from "../pages/DashboardPage"
 import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
+import { Outlet } from "react-router"
 
 const DashboardLayout = () => {
 
   return (
-    <>
+    <div className="">
       <Header />
-      <DashboardPage />
-    </>
+      <div className="flex">
+        <Sidebar />
+        <DashboardPage />
+      </div>
+    </div>
   )
 }
 
