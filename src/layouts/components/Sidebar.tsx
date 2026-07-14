01cbe9d6a173d71da900cdb366ee287afc9136a4
devfaf <104205080+devfaf@users.noreply.github.com>
2026-07-14 15:47:54 +0330

    style: for sidebar

--- src/layouts/components/Sidebar.tsx
+++ src/layouts/components/Sidebar.tsx
@@ -2,7 +2,25 @@
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className="w-3xs relative">
+      <div className="w-3xs z-40 bg-gray-200 shadow-lg overflow-y-auto fixed bottom-0 top-0 flex pt-20">
+        <div>
+          sidebar
+
+        </div>
+        <ul>
+          <li>
+            <a href="">داشبورد</a>
+          </li>
+          <li>
+            <a href="">تنظیمات</a>
+          </li>
+          <li>
+            <a href="">آمار</a>
+          </li>
+        </ul>
+      </div>
+    </aside>
   )
 }
 
