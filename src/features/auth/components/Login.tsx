70b6d89e1e649ab1337d5a96200b76e4403438f8
devfaf <104205080+devfaf@users.noreply.github.com>
2026-07-27 16:55:57 +0330

    feat: add spinner to login button

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -2,47 +2,67 @@ import Input from "../../../components/common/Input"
 import Button from "../../../components/common/Button"
 import { useAuthStore } from "../../auth/authStore"
 import { useNavigate } from "react-router"
+import { useState } from "react"
+import { ImSpinner2 } from "react-icons/im";
 
 const Login = () => {
     const login = useAuthStore(state => state.login)
     const navigate = useNavigate()
+    const [isLoading, setIsLoading] = useState(false);
 
     const submitHandler = (e: React.SubmitEvent) => {
         e.preventDefault()
 
-        login()
+        setIsLoading(true)
 
-        navigate("/profile")
+        setTimeout(() => {
+            login()
+            navigate("/profile")
+        }, 700);
     }
 
     return (
-            <form
-                onSubmit={submitHandler}
-                action=""
-                className="bg-gray-100 w-md border-gray-300 border flex flex-col items-center justify-center gap-4 p-4 rounded-lg">
-                <h2 className="text-2xl font-bold">فرم لاگین</h2>
-                <div className="w-full">
-                    <Input
-                        placeholder="ایمیل"
-                        label="ایمیل"
-                        type="email"
-                        className={`bg-gray-100 border-2 border-gray-300 outline-none rounded-lg px-2 w-full`}
-                    />
-                </div>
-                <div className="w-full">
-                    <Input
-                        placeholder="پسورد"
-                        label="پسورد"
-                        type="password"
-                        className={`bg-gray-100 border-2 border-gray-300 outline-none rounded-lg px-2 w-full`}
-                    />
-                </div>
-                <div className="w-full">
-                    <Button
-                        type="submit"
-                        className="bg-blue-500 hover:bg-blue-700 duration-300 rounded-lg p-2 text-white cursor-pointer w-full">ثبت نام</Button>
-                </div>
-            </form>
+        <form
+            onSubmit={submitHandler}
+            action=""
+            className="bg-gray-100 w-md border-gray-300 border flex flex-col items-center justify-center gap-4 p-4 rounded-lg">
+            <h2 className="text-2xl font-bold">فرم لاگین</h2>
+            <div className="w-full">
+                <Input
+                    placeholder="ایمیل"
+                    label="ایمیل"
+                    type="email"
+                    className={`bg-gray-100 border-2 border-gray-300 outline-none rounded-lg px-2 w-full`}
+                />
+            </div>
+            <div className="w-full">
+                <Input
+                    placeholder="پسورد"
+                    label="پسورد"
+                    type="password"
+                    className={`bg-gray-100 border-2 border-gray-300 outline-none rounded-lg px-2 w-full`}
+                />
+            </div>
+            <div className="w-full">
+                <Button
+                    type="submit"
+                    disabled={isLoading}
+                    className="bg-blue-500 hover:bg-blue-700 duration-300 rounded-lg p-2 text-white cursor-pointer w-full">
+
+                    {
+                        isLoading ? (
+                            <div className="flex gap-2 items-center justify-center">
+                                <span>درحال ورود به پنل کاربری...
+                                </span>
+                                    <ImSpinner2 className="animate-spin text-lg" />
+                            </div>
+                        ) : (
+                            <span>ورود | ثبت نام</span>
+                        )
+                    }
+                </Button>
+            </div>
+        </form>
     )
 }
 
