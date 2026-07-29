f484f364515b05da1f226facb6a544896ef39d7a
devfaf <104205080+devfaf@users.noreply.github.com>
2026-07-29 23:24:24 +0330

    style: add larget text size

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -1,20 +1,20 @@
 import { useParams } from "react-router"
 import { useTaskStore } from "../features/projects/store/taskStore"
-
+import { useProjectStore } from "../features/projects/store/projectStore"
 const OverviewPage = () => {
   const { id } = useParams()
 
   const tasks = useTaskStore(state => state.tasks)
-  // const projects = useProjectStore(state => state.projects)
+  const projects = useProjectStore(state => state.projects)
 
   const taskCount = tasks.filter(task => task.projectId === Number(id))
   const todoTaskCount = taskCount.filter(task => task.status === "todo")
   const doneTaskCount = taskCount.filter(task => task.status === "done")
   const inProgressTaskCount = taskCount.filter(task => task.status === "inProgress")
   const reviewTaskCount = taskCount.filter(task => task.status === "review")
+  const projectStatus = projects.filter(project => project.id === Number(id))[0].status
+  // console.log(projectStatus);
 
-
-  console.log(doneTaskCount);
 
   return (
     <div className="grid gap-4 md:grid-cols-6 p-4">
@@ -25,13 +25,17 @@ const OverviewPage = () => {
           border-[var(--color-border)]
           bg-[var(--color-surface)]
           p-3
+          flex
+          flex-col
+          items-center
+          h-full
         "
       >
-        <p className="mb-2 text-xs text-[var(--color-text-secondary)] text-center pb-2">
+        <p className="text-xs text-[var(--color-text-secondary)] text-center">
           تعداد کل تسک‌ها
         </p>
 
-        <h2 className="text-4xl font-bold text-[var(--color-primary)] text-center">
+        <h2 className="text-6xl font-bold h-full flex items-center text-[var(--color-primary)] text-center">
           {taskCount.length}
         </h2>
       </div>
@@ -43,13 +47,17 @@ const OverviewPage = () => {
           border-[var(--color-border)]
           bg-[var(--color-surface)]
           p-3
+          flex
+          flex-col
+          items-center
+          h-full
         "
       >
-        <p className="mb-2 text-xs text-[var(--color-text-secondary)] text-center pb-2">
+        <p className="text-xs text-[var(--color-text-secondary)] text-center">
           تسک‌های انجام‌نشده
         </p>
 
-        <h2 className="text-4xl font-bold text-[var(--color-danger)] text-center">
+        <h2 className="text-6xl font-bold h-full flex items-center text-[var(--color-danger)] text-center">
           {todoTaskCount.length}
         </h2>
       </div>
@@ -61,13 +69,17 @@ const OverviewPage = () => {
           border-[var(--color-border)]
           bg-[var(--color-surface)]
           p-3
+          flex
+          flex-col
+          items-center
+          h-full
         "
       >
-        <p className="mb-2 text-xs text-[var(--color-text-secondary)] text-center pb-2">
+        <p className="text-xs text-[var(--color-text-secondary)] text-center">
           تسک‌های درحال انجام
         </p>
 
-        <h2 className="text-4xl font-bold text-[var(--color-warning)] text-center">
+        <h2 className="text-6xl font-bold h-full flex items-center text-[var(--color-warning)] text-center">
           {inProgressTaskCount.length}
         </h2>
       </div>
@@ -79,13 +91,17 @@ const OverviewPage = () => {
           border-[var(--color-border)]
           bg-[var(--color-surface)]
           p-3
+          flex
+          flex-col
+          items-center
+          h-full
         "
       >
-        <p className="mb-2 text-xs text-[var(--color-text-secondary)] text-center pb-2">
+        <p className="text-xs text-[var(--color-text-secondary)] text-center">
           تسک‌های درحال بازبینی
         </p>
 
-        <h2 className="text-4xl font-bold text-[var(--color-info)] text-center">
+        <h2 className="text-6xl font-bold h-full flex items-center text-[var(--color-info)] text-center">
           {reviewTaskCount.length}
         </h2>
       </div>
@@ -97,15 +113,20 @@ const OverviewPage = () => {
           border-[var(--color-border)]
           bg-[var(--color-surface)]
           p-3
+          flex
+          flex-col
+          items-center
+          h-full
         "
       >
-        <p className="mb-2 text-xs text-[var(--color-text-secondary)] text-center pb-2">
+        <p className="text-xs text-[var(--color-text-secondary)] text-center">
           تسک‌های انجام‌شده
         </p>
 
-        <h2 className="text-4xl font-bold text-[var(--color-success)] text-center">
+        <h2 className="text-6xl font-bold text-[var(--color-success)] text-center h-full flex items-center">
           {doneTaskCount.length}
         </h2>
+
       </div>
 
       <div
@@ -115,9 +136,13 @@ const OverviewPage = () => {
           border-[var(--color-border)]
           bg-[var(--color-surface)]
           p-3
+          flex
+          flex-col
+          items-center
+          h-full
         "
       >
-        <p className="mb-2 text-xs text-[var(--color-text-secondary)] text-center pb-2">
+        <p className="text-xs text-[var(--color-text-secondary)] text-center">
           وضعیت پروژه‌ها
         </p>
 
@@ -128,18 +153,24 @@ const OverviewPage = () => {
             
             ">
           <span
-            className="
+            className={`
+              ${projectStatus === "completed" ?
+                "bg-green-100" :
+                "bg-red-100"
+              }
               inline-flex
               rounded-lg
-              bg-green-100
-              py-3
-              px-6
+              py-2
+              px-4
+              mt-2
               text-3xl
               text-green-700
               text-center
-            "
+            `}
           >
-            فعال
+            {
+              projectStatus === "completed" ? "تکمیل شده" : "فعال"
+            }
           </span>
         </div>
 
